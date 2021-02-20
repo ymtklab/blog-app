f2bac641fce7b2400e010c22ddc3d9f6190f216f
ymtk <ymuc10tkm18@outlook.jp>
2021-02-20 11:56:34 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/blog_data'
-);
+ );
 module.exports = {
   database: sequelize,
   Sequelize: Sequelize
